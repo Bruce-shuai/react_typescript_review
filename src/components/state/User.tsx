27c0a979fd70a0f134a2a50b87978141a261ce14
帅得乱七八糟 <284970670@qq.com>
2021-12-22 10:16:50 +0800

    对于useState使用类型断言

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -6,25 +6,22 @@ type AuthUser = {
 }
 
 export const User = () => {
-  // 兼容多种情况的类型，用泛型
-  const [user, setUser] = useState<AuthUser | null>();
+  // 这里使用类型断言，相当于告诉编译器，我比你更懂类型会是怎样的
+  // 这里相当于对象只能是 AuthUser类型的
+  const [user, setUser] = useState<AuthUser>({} as AuthUser);  
   const handleLogin = () => {
     setUser({
       name: 'Bruce',
       email: '284970670@qq.com'
     })
   }
-  const handleLogout = () => {
-    setUser(null)
-  }
 
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
-      {/* 因为user的类型可以是null，所以这里使用?.的操作 */}
-      <div>User name is {user?.name}</div>
-      <div>User email is {user?.email}</div>
+      {/* 避免总是要先假定user可能是null这种情况 */}
+      <div>User name is {user.name}</div>
+      <div>User email is {user.email}</div>
     </div>
   )
 }
